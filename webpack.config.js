b8144180aafa125a6acfb7396108ecb7dd5a0316
gabei <gabe1792@gmail.com>
2021-12-21 14:42:55 -0600

    setup webpack config and basic build command

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,9 @@
 const path = require('path');
 
-export.modules = {
-  
-}
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
