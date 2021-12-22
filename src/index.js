18e91c94a83a6e446d7f1e9902ca87b6aec6a11f
gabei <gabe1792@gmail.com>
2021-12-22 16:16:29 -0600

    began adding info section. adjusted where the clearfix mixin is being placed. now added to the parent container to clear all floats inside itself

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ document.body.appendChild(content);
 /*********************/
 /****** Header *******/
 /*********************/
-const header = createElement('div', 'header');
+const header = createElement('section', 'header');
 content.appendChild(header);
 
 /* logo */
@@ -37,7 +37,7 @@ header.appendChild(menu);
 /*******************/
 /****** Hero *******/
 /*******************/
-const hero = createElement('div', 'hero');
+const hero = createElement('section', 'hero');
 const heroText = createElement('div', 'hero__text');
 
 const heroTextContent = createElement('h1');
@@ -50,3 +50,60 @@ content.append(hero);
 /*******************/
 /****** Info *******/
 /*******************/
+
+const info = createElement('section', 'info');
+content.append(info);
+
+const infoText = createElement('div', 'info__text');
+info.append(infoText);
+
+const infoTextScheduleHeading = createElement('h3');
+infoTextScheduleHeading.textContent = 'Hours of Operation';
+
+const infoTextSchedule = createElement('ul');
+const infoTextScheduleContent = [
+  'Monday: 8am - 8pm',
+  'Tuesday: 8am - 8pm',
+  'Wednesday: 8am - 8pm',
+  'Thursday: 8am - 8pm',
+  'Friday: 8am - 8pm',
+  'Saturday: 8am - 8pm',
+  'Sunday: Closed',
+];
+
+for (let item of infoTextScheduleContent) {
+  let li = createElement('li');
+  li.textContent = item;
+  infoTextSchedule.append(li);
+}
+
+infoText.append(infoTextScheduleHeading, infoTextSchedule);
+
+const infoMap = createElement('div', 'info__map');
+info.append(infoMap);
+
+/******************/
+/****** CTA *******/
+/******************/
+const orderOnline = createElement('section', 'order-online');
+content.append(orderOnline);
+
+const orderOnlineText = createElement('div', 'order-online__text');
+orderOnline.append(orderOnlineText);
+
+const orderOnlineTextHeader = createElement('h2');
+orderOnlineTextHeader.textContent = "Order online or don't. Whatever";
+
+const orderOnlineTextPara = createElement('p');
+orderOnlineTextPara.textContent =
+  'Halvah icing wafer jelly donut chupa chups dragée cookie bear claw. Biscuit cake topping cookie oat cake brownie. Gummi bears oat cake lemon dro;ps carrot cake fruitcake. Jelly-o candy canes sugar plum dessert halvah icing dessert oat cake.';
+
+const orderOnlineButton = createElement('a', 'order-online__button');
+orderOnlineButton.textContent = 'Order Now';
+orderOnlineButton.attributes['href'] = '#';
+
+orderOnlineText.append(
+  orderOnlineTextHeader,
+  orderOnlineTextPara,
+  orderOnlineButton
+);
