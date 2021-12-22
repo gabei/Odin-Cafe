d705fcf0f0444edbe7188a57ff52e5a6e9667f45
gabei <gabe1792@gmail.com>
2021-12-22 12:21:44 -0600

    began setup of DOM elements generated by JS after deleting old index.html file...

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
-console.log('It worked!');
+const content = createElement('div', 'content');
+document.body.appendChild(content);
 
-const content = document.getElementById('content');
+const header = createElement('div', ['header']);
+content.appendChild(header);
+
+/* DOM Creation */
+
+function createElement(type = 'div', [...classes]) {
+  const newElement = document.createElement(type);
+  addClasses(newElement, classes);
+
+  return newElement;
+}
+
+function addClasses(element, [...classes]) {
+  classes.forEach((newClass) => {
+    element.classList.add(newClass);
+  });
+}
