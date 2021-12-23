549dcc3690325dbc442bff2eb7b0b83eeca80a54
gabei <gabe1792@gmail.com>
2021-12-23 10:51:55 -0600

    updated webpack for image loae loading. Added masonry image section.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import { createElement } from './DOM/DOM.js';
 import './style.scss';
 
+import Image1 from './assets/images/cafe1.jpg';
+import Image2 from './assets/images/cafe2.jpg';
+import Image3 from './assets/images/cafe3.jpg';
+import Image4 from './assets/images/cafe4.jpg';
+import Image5 from './assets/images/cafe5.jpg';
+
 const content = createElement('div', 'content');
 document.body.appendChild(content);
 
@@ -111,5 +117,34 @@ orderOnlineText.append(
 /*********************/
 /****** Images *******/
 /*********************/
-const images = createElement('section', 'images');
-content.append(images);
+/* Please refer to this great codepen for more understanding on this masonry image setup. This is only a slightly modified version of this original: https://codepen.io/iamsaief/pen/jObaoKo */
+
+const imageMason = createElement('section', 'image-mason');
+content.append(imageMason);
+
+const images = [
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+  Image1,
+  Image2,
+  Image3,
+  Image4,
+  Image5,
+];
+
+for (let src of images) {
+  let image = createElement('img');
+  image.src = src;
+
+  let div = createElement('div');
+  div.append(image);
+
+  imageMason.append(div);
+}
+
+/*********************/
+/****** Footer *******/
+/*********************/
