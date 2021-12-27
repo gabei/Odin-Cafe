442abcc566a98336b023d357ae5a0c60b992877f
gabei <gabe1792@gmail.com>
2021-12-27 15:58:45 -0600

    tested out loading just the menu module into the index.js. Also added styling for a new header type

--- src/Menu/Menu.js
+++ src/Menu/Menu.js
@@ -1,1 +1,15 @@
 import { createElement } from '../DOM/DOM';
+import Header from '../Header/Header';
+
+const Menu = createElement('div', 'Menu');
+
+const menuHeader = createElement('section', 'Menu__header');
+menuHeader.append(Header);
+
+const menuHeaderText = createElement('h1', 'Menu__header--text');
+menuHeaderText.textContent = 'Our Menu';
+menuHeader.append(menuHeaderText);
+
+Menu.append(menuHeader);
+
+export default Menu;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,10 @@
 import { createElement } from './DOM/DOM.js';
 
 import Home from './Home/Home';
+import Menu from './Menu/Menu';
 import './style.scss';
 
 const Content = createElement('div', 'content');
 document.body.append(Content);
 
-Content.append(Home);
+Content.append(Menu);
