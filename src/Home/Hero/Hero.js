ca74759efa32ba3ab0a44e6b258dfe8fb4b63756
gabei <gabe1792@gmail.com>
2021-12-30 13:48:10 -0600

    added a button to the herp and a separate alternate button mixin

--- src/Home/Hero/Hero.js
+++ src/Home/Hero/Hero.js
@@ -6,7 +6,10 @@ const heroText = createElement('div', 'hero__text');
 const heroTextContent = createElement('h1');
 heroTextContent.textContent = 'Here is a promise that we will keep.';
 
+const heroTextButton = createElement('a', 'hero__text-button');
+heroTextButton.textContent = 'View our menu';
+
+heroText.append(heroTextContent, heroTextButton);
 Hero.append(heroText);
-heroText.append(heroTextContent);
 
 export default Hero;
